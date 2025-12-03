609b221d457eb9992a2a40afe5421ac8809f9e7f
bagastiaadi <imadebagastiaadi@gmail.com>
2025-12-03 15:36:45 +0800

    Menambahkan fungsi update dan detele

--- class/Book.php
+++ class/Book.php
@@ -27,4 +27,26 @@ class Book {
             ':status' => $data['status']
         ]);
     }
+
+    public function update($id, $data) {
+        $db = Database::connect();
+        $sql = "UPDATE books SET title=:title, author=:author, year=:year, category=:category, cover=:cover, status=:status WHERE id=:id";
+        $stmt = $db->prepare($sql);
+        $data['id'] = $id;
+        return $stmt->execute([
+            ':title' => $data['title'],
+            ':author' => $data['author'],
+            ':year' => $data['year'],
+            ':category' => $data['category'],
+            ':cover' => $data['cover'],
+            ':status' => $data['status'],
+            ':id' => $data['id']
+        ]);
+    }
+
+    public function delete($id) {
+        $db = Database::connect();
+        $stmt = $db->prepare("DELETE FROM books WHERE id = ?");
+        return $stmt->execute([(int)$id]);
+    }
 }
