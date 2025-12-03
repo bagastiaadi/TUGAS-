331b6312482d19f9d0951a0754f4a29011064453
bagastiaadi <imadebagastiaadi@gmail.com>
2025-12-03 14:26:30 +0800

    Menambahakan fungsi insert data atau create pada file Book.php

--- class/Book.php
+++ class/Book.php
@@ -1,10 +1,30 @@
 <?php
-
 class Book {
 
     public function getAll() {
         $db = Database::connect();
         $stmt = $db->query("SELECT * FROM books ORDER BY id DESC");
         return $stmt->fetchAll();
+    }
+
+    public function find($id) {
+        $db = Database::connect();
+        $stmt = $db->prepare("SELECT * FROM books WHERE id = ?");
+        $stmt->execute([(int)$id]);
+        return $stmt->fetch();
+    }
+
+    public function insert($data) {
+        $db = Database::connect();
+        $sql = "INSERT INTO books (title, author, year, category, cover, status) VALUES (:title,:author,:year,:category,:cover,:status)";
+        $stmt = $db->prepare($sql);
+        return $stmt->execute([
+            ':title' => $data['title'],
+            ':author' => $data['author'],
+            ':year' => $data['year'],
+            ':category' => $data['category'],
+            ':cover' => $data['cover'],
+            ':status' => $data['status']
+        ]);
     }
 }
